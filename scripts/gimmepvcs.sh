8b516758f426fcb9b688da28a4d04f9a682f0f44
Rojikku <Rojikku@users.noreply.github.com>
2022-01-09 21:19:37 -0500

    Add new pvcs

--- scripts/gimmepvcs.sh
+++ scripts/gimmepvcs.sh
@@ -3,3 +3,7 @@ kubectl apply -f cluster/apps/hass/zwave/config-pvc.yaml
 kubectl apply -f cluster/apps/hass/nodered/config-pvc.yaml
 kubectl apply -f cluster/apps/hass/mosquitto/config-pvc.yaml
 kubectl apply -f cluster/apps/hass/home-assistant/config-pvc.yaml
+kubectl apply -f cluster/apps/sonarr/jackett/config-pvc.yaml
+kubectl apply -f cluster/apps/sonarr/prowlarr/config-pvc.yaml
+kubectl apply -f cluster/apps/sonarr/qbittorrent/config-pvc.yaml
+kubectl apply -f cluster/apps/sonarr/sonarr/config-pvc.yaml
