dc5f04038043c2d273886f7027797b9258c3aec5
Rojikku <Rojikku@users.noreply.github.com>
2022-01-12 19:17:59 -0500

    Unnecessary script

--- scripts/initdbs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-kubectl apply -f ./cluster/apps/networking/authentik/postgres/postgres-db.yaml
-kubectl apply -f ./cluster/apps/hass/home-assistant/postgres-db.yaml
-kubectl apply -f ./cluster/apps/monitoring/grafana/postgres-db.yaml
