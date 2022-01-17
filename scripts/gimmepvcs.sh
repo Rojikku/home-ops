a9f910a55a0183530af5baadf2fd238ab2e66661
Rojikku <Rojikku@users.noreply.github.com>
2022-01-17 18:48:46 -0500

    Note new PVC

--- scripts/gimmepvcs.sh
+++ scripts/gimmepvcs.sh
@@ -7,3 +7,4 @@ kubectl apply -f cluster/apps/sonarr/jackett/config-pvc.yaml
 kubectl apply -f cluster/apps/sonarr/prowlarr/config-pvc.yaml
 kubectl apply -f cluster/apps/sonarr/qbittorrent/config-pvc.yaml
 kubectl apply -f cluster/apps/sonarr/sonarr/config-pvc.yaml
+kubectl apply -f cluster/apps/stash/backup-pvc.yaml
