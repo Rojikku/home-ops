c4e047c845211858c4ad28a9bb528c9014748705
Rojikku <Rojikku@users.noreply.github.com>
2022-02-22 22:02:55 -0500

    Feat: Update PVC locations

--- scripts/gimmepvcs.sh
+++ scripts/gimmepvcs.sh
@@ -3,8 +3,8 @@ kubectl apply -f cluster/apps/hass/zwave/config-pvc.yaml
 kubectl apply -f cluster/apps/hass/nodered/config-pvc.yaml
 kubectl apply -f cluster/apps/hass/mosquitto/config-pvc.yaml
 kubectl apply -f cluster/apps/hass/home-assistant/config-pvc.yaml
-kubectl apply -f cluster/apps/sonarr/jackett/config-pvc.yaml
-kubectl apply -f cluster/apps/sonarr/prowlarr/config-pvc.yaml
-kubectl apply -f cluster/apps/sonarr/qbittorrent/config-pvc.yaml
-kubectl apply -f cluster/apps/sonarr/sonarr/config-pvc.yaml
+kubectl apply -f cluster/apps/vpn/jackett/config-pvc.yaml
+kubectl apply -f cluster/apps/vpn/prowlarr/config-pvc.yaml
+kubectl apply -f cluster/apps/vpn/qbittorrent/config-pvc.yaml
+kubectl apply -f cluster/apps/vpn/sonarr/config-pvc.yaml
 kubectl apply -f cluster/apps/networking/authentik/backup-pvc.yaml
