b6ec32a011a406c88e6fa483dc175d68912062b5
Rojikku <Rojikku@users.noreply.github.com>
2022-12-17 14:03:23 -0500

    cluster: Update script with pvcs

--- scripts/gimmepvcs.sh
+++ scripts/gimmepvcs.sh
@@ -3,8 +3,11 @@ kubectl apply -f cluster/apps/hass/zwave/config-pvc.yaml
 kubectl apply -f cluster/apps/hass/nodered/config-pvc.yaml
 kubectl apply -f cluster/apps/hass/mosquitto/config-pvc.yaml
 kubectl apply -f cluster/apps/hass/home-assistant/config-pvc.yaml
-kubectl apply -f cluster/apps/vpn/jackett/config-pvc.yaml
-kubectl apply -f cluster/apps/vpn/prowlarr/config-pvc.yaml
-kubectl apply -f cluster/apps/vpn/qbittorrent/config-pvc.yaml
-kubectl apply -f cluster/apps/vpn/sonarr/config-pvc.yaml
+
+kubectl apply -f cluster/apps/media/prowlarr/config-pvc.yaml
+kubectl apply -f cluster/apps/media/qbittorrent/config-pvc.yaml
+kubectl apply -f cluster/apps/media/sonarr/config-pvc.yaml
+kubectl apply -f cluster/apps/media/jellyfin/config-pvc.yaml
+kubectl apply -f cluster/apps/media/ombi/config-pvc.yaml
+
 kubectl apply -f cluster/apps/networking/authentik/backup-pvc.yaml
