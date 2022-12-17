0eb087e1d1556d73e459b4bc8e9aa98b1a80d409
Rojikku <Rojikku@users.noreply.github.com>
2022-12-17 12:44:22 -0500

    stash: Remove unused

--- stash-restore/restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-
-# Comment as desired
-kubectl apply -f restores/hass.yaml
-kubectl apply -f restores/nodered.yaml
-kubectl apply -f restores/zwavejs.yaml
