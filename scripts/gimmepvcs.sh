21551224bd7fd2d19958107ada89c972b2485895
Rojikku <Rojikku@users.noreply.github.com>
2023-11-14 14:35:19 -0500

    Scripts: Update pvc paths

--- scripts/gimmepvcs.sh
+++ scripts/gimmepvcs.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-kubectl apply -f cluster/apps/hass/zwave/config-pvc.yaml
-kubectl apply -f cluster/apps/hass/nodered/config-pvc.yaml
-kubectl apply -f cluster/apps/hass/mosquitto/config-pvc.yaml
-kubectl apply -f cluster/apps/hass/home-assistant/config-pvc.yaml
+kubectl apply -f cluster/apps/hass/zwave/app/config-pvc.yaml
+kubectl apply -f cluster/apps/hass/nodered/app/config-pvc.yaml
+kubectl apply -f cluster/apps/hass/home-assistant/app/config-pvc.yaml
 
-kubectl apply -f cluster/apps/media/prowlarr/config-pvc.yaml
-kubectl apply -f cluster/apps/media/qbittorrent/config-pvc.yaml
-kubectl apply -f cluster/apps/media/sonarr/config-pvc.yaml
-kubectl apply -f cluster/apps/media/jellyfin/config-pvc.yaml
-kubectl apply -f cluster/apps/media/ombi/config-pvc.yaml
+kubectl apply -f cluster/apps/default/mealie/api/config-pvc.yaml
+
+kubectl apply -f cluster/apps/media/prowlarr/app/config-pvc.yaml
+kubectl apply -f cluster/apps/media/qbittorrent/app/config-pvc.yaml
+kubectl apply -f cluster/apps/media/sonarr/app/config-pvc.yaml
+kubectl apply -f cluster/apps/media/radarr/app/config-pvc.yaml
+kubectl apply -f cluster/apps/media/jellyfin/app/config-pvc.yaml
+kubectl apply -f cluster/apps/media/ombi/app/config-pvc.yaml
+kubectl apply -f cluster/apps/media/aniplanrr/app/config-pvc.yaml
 
 kubectl apply -f cluster/apps/networking/authentik/backup-pvc.yaml
