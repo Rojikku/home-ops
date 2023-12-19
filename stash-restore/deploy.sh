69b3f5b55d81569e71b3a985d16dcbee9046613c
Rojikku <Rojikku@users.noreply.github.com>
2023-12-19 10:23:57 -0500

    Remove old stash data

--- stash-restore/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-
-# Comment as desired
-kubectl apply -f deployments/hass.yaml
-kubectl apply -f deployments/nodered.yaml
-kubectl apply -f deployments/zwavejs.yaml
